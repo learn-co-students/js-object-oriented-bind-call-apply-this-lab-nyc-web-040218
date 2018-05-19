6ba5c4290c93eb9c45d3b88b29211cb928ffd50c
William Dale <williambdale@gmail.com>
2018-05-19 16:40:09 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, value, arg) {
+  return fn.call(value, arg)
+}
+
+function setThisWithApply(fn, value, args) {
+  return fn.apply(value, args)
+}
+
+function returnNewFunctionOf(copyFn, value) {
+  return Object.assign(copyFn).bind(value)
+}
